0c715cc7aadff4d62583cc6ef3cfe20293e6580d
iamnewneo <kunalgusain1995@gmail.com>
2021-09-30 12:36:22 -0400

    Legacy Bug Fix CNN

--- rl_space/legacy_code/train_cnn.py
+++ rl_space/legacy_code/train_cnn.py
@@ -199,11 +199,181 @@ class CNNTrainer:
             json.dump(tmtr_panels, f)
 
 
+class CNNTest:
+    def __init__(self) -> None:
+        self.model = CNNModel()
+        self.model.load_state_dict(
+            torch.load("./models/cnn_model.pth", map_location=torch.device(DEVICE))
+        )
+
+    def test_cnn(self):
+        cap = cv2.VideoCapture("./data/atari_2_vehicles.avi")
+
+        fps = int(cap.get(1))
+
+        frame_width = int(cap.get(3))
+
+        frame_height = int(cap.get(4))
+        tankrc = []
+        tankm = []
+
+        # tank_moving_panels = {
+        #     0: 2939.852294921875,
+        #     1: 3651.266845703125,
+        #     2: 3918.51708984375,
+        #     4: 2535.77880859375,
+        #     6: 2757.001953125,
+        #     8: 1897.1790771484375,
+        #     12: 2739.127685546875,
+        #     15: 2944.8779296875,
+        # }
+
+        # tank_reloading_panels = {
+        #     3: 2042.2137451171875,
+        #     5: 2765.12841796875,
+        #     7: 3176.161376953125,
+        #     10: 2638.602783203125,
+        #     11: 3490.5673828125,
+        #     14: 1660.1922607421875,
+        # }
+
+        # tmtr_panels = {9: 2719.021484375, 13: 3320.355712890625}
+
+        with open("./data/tank_moving_panels.json", "r") as f:
+            tank_moving_panels = json.load(f)
+        with open("./data/tank_reloading_panels.json", "r") as f:
+            tank_reloading_panels = json.load(f)
+        with open("./data/tmtr_panels.json", "r") as f:
+            tmtr_panels = json.load(f)
+
+        tank_moving_panels = {int(k): v for k, v in tank_moving_panels.items()}
+        tank_reloading_panels = {int(k): v for k, v in tank_reloading_panels.items()}
+        tmtr_panels = {int(k): v for k, v in tmtr_panels.items()}
+
+        keys = list(tank_reloading_panels.keys())
+        keys_tmtr = list(tmtr_panels.keys())
+        keys_tm = list(tank_moving_panels.keys())
+
+        uframes = []
+        # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
+
+        out = cv2.VideoWriter(
+            "Two Aliens.avi",
+            cv2.VideoWriter_fourcc("M", "J", "P", "G"),
+            10,
+            (frame_width, frame_height),
+        )
+
+        def val_pos(array):
+            value = np.sum(array)
+            position = np.unravel_index(np.argmax(array), array.shape)
+            # print(f"position: {position}")
+            return value, position
+
+        ret, frame = cap.read()
+
+        while ret:
+            # Capture frames in the video
+            # describe the type of font
+            # to be used.
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            # Use putText() method for
+            # inserting text on video
+            # print(np.shape(frame))
+            uframes = []
+            uframes.append(cv2.resize(frame, (100, 100)))
+            img = uframes[0]
+
+            data3 = []
+            data3.append(
+                np.array(np.transpose(np.asarray(img), (2, 0, 1)), dtype=np.float32)
+            )
+            y3 = np.array(data3)
+            x_1 = torch.from_numpy(y3)
+            output = self.model(x_1.reshape(1, 3, 100, 100))[0].detach().numpy()
+
+            for key in keys:
+                output_tankr = output[0][key]
+                val_tankr, pos_tankr = val_pos(output_tankr)
+                if val_tankr > tank_reloading_panels[key]:
+                    # print("tank reloading present")
+                    tankrc.append(pos_tankr)
+                    # l.append(i)
+                    # Display the resulting frame
+                    cv2.putText(
+                        frame,
+                        "Fire Here",
+                        (73 * pos_tankr[1], 98 * pos_tankr[0]),
+                        font,
+                        2,
+                        (0, 165, 255),
+                        5,
+                        cv2.LINE_4,
+                    )
+                    cv2.putText(
+                        frame,
+                        "Take Action: The Vehicle is going to fire",
+                        (50, 50),
+                        font,
+                        2,
+                        (0, 0, 255),
+                        5,
+                        cv2.LINE_4,
+                    )
+
+            for key in keys_tmtr:
+                output_tankr = output[0][key]
+                val_tankr, pos_tankr = val_pos(output_tankr)
+                if val_tankr > tmtr_panels[key]:
+                    # print("tank reloading present")
+                    tankrc.append(pos_tankr)
+                    # l.append(i)
+                    # Display the resulting frame
+                    cv2.putText(
+                        frame,
+                        "Fire Here",
+                        (73 * pos_tankr[1], 98 * pos_tankr[0]),
+                        font,
+                        2,
+                        (0, 165, 255),
+                        5,
+                        cv2.LINE_4,
+                    )
+                    cv2.putText(
+                        frame,
+                        "Take Action: The Vehicle is going to fire",
+                        (50, 50),
+                        font,
+                        2,
+                        (0, 0, 255),
+                        5,
+                        cv2.LINE_4,
+                    )
+
+            for key in keys_tm:
+                output_tankr = output[0][key]
+                val_tankr, pos_tankr = val_pos(output_tankr)
+                if val_tankr > tank_moving_panels[key]:
+                    # print("tank reloading present")
+                    tankm.append(pos_tankr)
+                    # l.append(i)
+                    # Display the resulting frame
+
+            out.write(frame)
+            ret, frame = cap.read()
+
+
 def main():
     cnn_trainer = CNNTrainer()
     model = cnn_trainer.train_cnn()
     cnn_trainer.post_processing()
 
 
+def test_cnn():
+    cnn_tester = CNNTest()
+    cnn_tester.test_cnn()
+
+
 if __name__ == "__main__":
     main()
+    # test_cnn()
